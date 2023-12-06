ff455e0cc0593b1f4bb72da713ce8b731cc9497f
Admin <admin@admins-mac-mini.mynetworksettings.com>
2023-12-06 16:25:20 -0700

    So I now have a description you can save on your user page, login and register work but I just need to go verify some of the other api calls but I did some shit with redoing the docker compose, adding a docker file, added a new table for the descriptions and so far everything is working. So if you try to go to any page without being logged in it will redirect you to the login

--- All_Project_Code/init_data/create.sql
+++ All_Project_Code/init_data/create.sql
@@ -3,6 +3,12 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password CHAR(60) NOT NULL,
+    password CHAR(60) NOT NULL
 );
 
+DROP TABLE IF EXISTS userPage;
+CREATE TABLE userPage (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    user_desc VARCHAR(256)
+);
--- All_Project_Code/src/index.js
+++ All_Project_Code/src/index.js
@@ -2,7 +2,6 @@
 // <!-- Section 1 : Import Dependencies -->
 // *****************************************************
 
-
 const express = require('express'); // To build an application server or API
 const path = require('path');
 const app = express();
@@ -11,6 +10,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcrypt'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part B.
+const sessionSecret = process.env.SESSION_SECRET;
 
 // Serve static files from the 'resource' directory
 app.use(express.static(path.join(__dirname, 'init_data')));
@@ -41,44 +41,43 @@ db.connect()
     console.log('ERROR:', error.message || error);
   });
 
-// *****************************************************
+/// *****************************************************
 // <!-- Section 3 : App Settings -->
 // *****************************************************
 
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs'); // set the view engine to EJS
-app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+// Parse JSON bodies (as sent by API clients)
+app.use(bodyParser.json());
+
+// Parse URL-encoded bodies (as sent by HTML forms)
 app.use(bodyParser.urlencoded({ extended: true }));
-// initialize session variables
+
+// Session middleware setup - this should be before your route handlers
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if you are using HTTPS
+}));
+
+// Serve static files from the 'init_data', 'resource', and 'views/pages' directories
 app.use(express.static(path.join(__dirname, 'init_data')));
-
-
-// Serve static files from the 'resource' directory
 app.use(express.static(path.join(__dirname, 'resource')));
-
-
 app.use(express.static(path.join(__dirname, 'views/pages')));
 
-const users = {
-  username: undefined,
-  id: undefined,
+// Authentication middleware for protected routes
+const auth = (req, res, next) => {
+  if (!req.session.users) {
+    return res.redirect('/login');
+  }
+  next();
 };
 
+// Apply the authentication middleware to the '/discover' route
+app.use('/discover', auth);
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
-    resave: false,
-  })
-);
-
-
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
 
 
 
@@ -97,56 +96,51 @@ app.get('/detail-product', (req, res) => {
 
 app.get('/login', (req, res) => {
   const registered = req.query.registered;
+  const redirectedFrom = req.query.redirectedFrom;
   const message = registered ? 'Registration successful. Have fun SkineeDipping!' : '';
-  res.render('pages/home', { message : message });
+  
+  // Pass the redirectedFrom parameter to the EJS template
+  res.render('pages/login', { message: message, redirectedFrom: redirectedFrom });
 });
+
 
 
 app.post('/login', async (req, res) => {
   const query = 'SELECT * FROM users WHERE username = $1';
   const username = req.body.username;
   const values = [username];
-  console.log(query);
-  console.log(username);
-  console.log(values);
 
   db.any(query, values)
       .then(async function (data) {
-          if(data.length > 0){
+          if (data.length > 0) {
               const match = await bcrypt.compare(req.body.password, data[0].password);
-              //console.log(match);
-              // console.log(req.body.password);
-              // console.log(data[0].password);
-              // console.log(req.body.password.trim() === data[0].password.trim());
-              // console.log(typeof req.body.password, typeof data[0].password);
-              // console.log([...req.body.password].map(c => c.charCodeAt(0)));
-              // console.log([...data[0].password].map(c => c.charCodeAt(0)));
-
               
-              if(match){
-                  users.username = data[0].username;
-                  req.session.users = users;
-                  //res.json({status: 'success', message: 'success'});
-                  req.session.save();
-                  res.redirect('/discover');
-              }
-              else{
-                  console.log('Login failed, please try again');
-                  //res.json({status: 'Invalid input', message: 'Invalid input'});
+              if (match) {
+                req.session.users = {
+                    username: data[0].username,
+                    id: data[0].id
+                };
+                req.session.save(() => {
+                    // Redirect based on the redirectedFrom parameter
+                    const redirectUrl = req.body.redirectedFrom === 'user' ? '/user' : '/discover';
+                    res.redirect(redirectUrl);
+                });
+            } else {
+                  console.log('Login failed: Incorrect password');
                   res.redirect('/login');
               }
-          }
-         else {
-              console.log('no user data', err);
+          } else {
+              console.log('Login failed: User not found');
               res.redirect('/register');
           }
-      
       })
       .catch((err) => {
-          console.log('login failed', err);
+          console.log('Login failed:', err);
           res.redirect('/login');
-      })
+      });
 });
+
+
 
 app.get('/register', (req, res) => {
   res.render('pages/register');
@@ -228,10 +222,51 @@ app.get('/detail_product/:id', async (req, res) => {
 
 
 
-app.get('/user', (req, res) => {
-  res.render('pages/user', {
-    username: req.session.users.username,
-  });
+app.get('/user', async (req, res) => {
+  if (!req.session.users || !req.session.users.id) {
+      // Redirect to login and pass a query parameter
+      return res.redirect('/login?redirectedFrom=user');
+  }
+
+  try {
+      const userDescription = await db.oneOrNone('SELECT user_desc FROM userPage WHERE user_id = $1', [req.session.users.id]);
+      res.render('pages/user', {
+          username: req.session.users.username,
+          userDescription: userDescription ? userDescription.user_desc : 'I like SkineeDipping'
+      });
+  } catch (error) {
+      console.error('Error fetching user description:', error);
+      res.redirect('/login');
+  }
+});
+
+
+
+app.post('/saveDescription', async (req, res) => {
+  if (!req.session.users || !req.session.users.id) {
+      return res.redirect('/login'); // redirect if not logged in
+  }
+
+  const userId = req.session.users.id;
+  const description = req.body.description;
+
+  try {
+      // Check if the user already has a description
+      const existingDesc = await db.oneOrNone('SELECT * FROM userPage WHERE user_id = $1', [userId]);
+
+      if (existingDesc) {
+          // Update the existing description
+          await db.none('UPDATE userPage SET user_desc = $1 WHERE user_id = $2', [description, userId]);
+      } else {
+          // Insert a new description
+          await db.none('INSERT INTO userPage (user_id, user_desc) VALUES ($1, $2)', [userId, description]);
+      }
+
+      res.redirect('/user'); // Redirect back to user page
+  } catch (error) {
+      console.error('Error saving description:', error);
+      res.status(500).send('Internal Server Error');
+  }
 });
 
 
@@ -258,7 +293,7 @@ app.get('/welcome', (req, res) => {
 app.get('/home', (req, res) => {
   // Check if user is logged in
   if (req.session.users) {
-    res.render('pages/home', { user: req.session.users }); // Render the home page
+    res.render('pages/home', { user: req.session.users }); // Render the 'home' view
   } else {
     res.redirect('/login'); // Redirect to login if not logged in
   }
@@ -421,12 +456,4 @@ module.exports = app.listen(3000);
 //module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
 
-const auth = (req, res, next) => {
-  if (!req.session.users) {
-    return res.redirect('/login');
-  }
-  next();
-};
 
-// Use this middleware for any routes that require authentication
-app.use('/discover', auth);
